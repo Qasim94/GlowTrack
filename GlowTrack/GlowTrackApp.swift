7b3147c91451857da203af7cd6f7b5324c757cb1
Qasim <qasim@Qasims-Air.fritz.box>
2025-10-03 18:12:42 +0200

    Initial commit: GlowTrack iOS app setup

---
+++ GlowTrack/GlowTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowTrackApp.swift
+//  GlowTrack
+//
+//  Created by Qasim on 28.09.25.
+//
+
+import SwiftUI
+
+@main
+struct GlowTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
